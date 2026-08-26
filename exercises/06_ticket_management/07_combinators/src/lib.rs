0adad8b1ee7d555f9a277329d034675307121125
Jalen Vaughn <jvspeed74@gmail.com>
2026-08-26 18:16:02 -0400

    Complete exercises -----------
    Ticket Management:
    - 07

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -1,3 +1,4 @@
+use std::slice::Iter;
 // TODO: Implement the `to_dos` method. It must return a `Vec` of references to the tickets
 //  in `TicketStore` with status set to `Status::ToDo`.
 use ticket_fields::{TicketDescription, TicketTitle};
@@ -5,6 +6,15 @@ use ticket_fields::{TicketDescription, TicketTitle};
 #[derive(Clone)]
 pub struct TicketStore {
     tickets: Vec<Ticket>,
+}
+
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = Iter<'a, Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.iter()
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -30,6 +40,32 @@ impl TicketStore {
 
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
+    }
+
+    pub fn to_dos_two(&self) -> Vec<&Ticket> {
+        /*
+        We own the tickets, the output cannot live longer than us. Should be handled by self.
+
+        Need to implement the IntoIterator so we are able to get the ref, not the owned.
+
+        Create a new vector appending a &Ticket for each Ticket
+        */
+        let mut out: Vec<&Ticket> = Vec::with_capacity(self.tickets.len());
+        for ticket in self.into_iter() {
+            if ticket.status == Status::ToDo {
+                out.push(ticket)
+            }
+        }
+        out
+    }
+
+    pub fn to_dos(&self) -> Vec<&Ticket> {
+        /*
+        Took the other todos and made it use the functional pattern
+        */
+        self.into_iter()
+            .filter(|&x| x.status == Status::ToDo)
+            .collect()
     }
 }
 
